13d83adc2b30753576ee3152bfb7c2411c953abc
Alexey Fishler <amberdance@yandex.ru>
2023-07-27 13:12:58 +0300

    Добавил проверку перед подключинием пролога битрикса

--- config/test_bootstrap.php
+++ config/test_bootstrap.php
@@ -9,4 +9,6 @@ const NO_KEEP_STATISTIC = true;
 const NOT_CHECK_PERMISSIONS = true;
 const LOG_FILENAME = 'php://stderr';
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+if (file_exists($bitrixPrologBefore = $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php")) {
+    require $bitrixPrologBefore;
+}
