646979b7aa27cbcc81e810c14ed5ac8f51a67be0
Alexey Fishler <amberdance@yandex.ru>
2023-08-10 15:15:00 +0300

    Добавил конструктор в CaptchaVerifyException.php

--- src/hrd2cd/Exception/CaptchaVerifyException.php
+++ src/hrd2cd/Exception/CaptchaVerifyException.php
@@ -3,8 +3,13 @@
 namespace Hard2Code\Exception;
 
 use Exception;
+use Throwable;
 
 class CaptchaVerifyException extends Exception
 {
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
 }
