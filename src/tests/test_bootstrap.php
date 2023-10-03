704e020dec9b6fe2c7ce62f869f76e23e26c14ac
Alexey Fishler <amberdance@yandex.ru>
2023-10-03 11:01:24 +0300

    Исправлена ошибка слияния

--- src/tests/test_bootstrap.php
+++ src/tests/test_bootstrap.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD:src/tests/config/test_bootstrap.php
-require __DIR__."/../../../vendor/autoload.php";
-=======
 require __DIR__."/../../vendor/autoload.php";
->>>>>>> c1cea50df1b95c54a486cd8f7d1c2d8d076a2d03:src/tests/test_bootstrap.php
 
 $_SERVER["DOCUMENT_ROOT"] = dirname(__DIR__, 3);
-
 
 const NO_KEEP_STATISTIC = true;
 const NOT_CHECK_PERMISSIONS = true;
